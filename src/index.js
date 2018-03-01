f647ae88515bf6fc9e85fff4f632578ed37b7557
Vitali Br <epamc@mail.ru>
2018-03-01 21:50:18 +0300

    max length in array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+  
+  var n = array.length;
+  var S = new Array();
+  var k =0;
+  
+  for (var i=0; i<n;i++) { 
+	  S[array[i]]=array[i];}
+  
+  for ( var i=0; i<n; i++) {
+	    
+	  if(S[array[i]-1] === undefined ) {
+		  var j= array[i];
+		  while (S[j]) j++
+
+		  if (k<j-array[i]) k = j-array[i];
+	  }
+  
+  } return k;
+}  
